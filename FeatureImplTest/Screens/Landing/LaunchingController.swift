fd0dafc03e5a0a8270bfd1cd4e261ffb9cd28773
Hugo Flores <hfp.ing2@gmail.com>
2020-05-20 14:53:05 -0400

    Side menu presentation and configuration

--- FeatureImplTest/Screens/Landing/LaunchingController.swift
+++ FeatureImplTest/Screens/Landing/LaunchingController.swift
@@ -6,19 +6,40 @@
 //  Copyright © 2020 Hugo Flores Perez. All rights reserved.
 //
 
+import SideMenu
 import UIKit
 
 class LaunchingController: UITabBarController {
 
-    private let tabControllers = [MapScreenController(), ChartTableScreenViewController()]
+    private let tabContentControllers = [MapScreenController(), ChartTableScreenViewController()]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setUpTabs()
+        setUpNavBar()
     }
 
     private func setUpTabs() {
-        viewControllers = tabControllers
+        viewControllers = tabContentControllers
+    }
+
+    private func setUpNavBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "ellipsis"), style: .plain, target: self, action: #selector(onMenuPressed))
+    }
+
+    @objc func onMenuPressed() {
+        let menu = SideMenuNavigationController(rootViewController: SideMenuViewController())
+        let presentationStyle: SideMenuPresentationStyle = .menuSlideIn
+        presentationStyle.backgroundColor = .systemBlue
+        presentationStyle.onTopShadowOpacity = 0.5
+        presentationStyle.menuStartAlpha = 0
+        presentationStyle.presentingEndAlpha = 0.5
+        var settings = SideMenuSettings()
+        settings.presentationStyle = presentationStyle
+        settings.statusBarEndAlpha = 0
+        menu.settings = settings
+        menu.leftSide = true
+        present(menu, animated: true, completion: nil)
     }
 }
