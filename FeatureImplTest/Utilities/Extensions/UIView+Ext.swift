16b1fb3f8607e4f261cd1b82b4b2b0a8d7aca2fb
Hugo Flores <hfp.ing2@gmail.com>
2020-05-20 11:08:55 -0400

    Add and fill parent view method extension

---
+++ FeatureImplTest/Utilities/Extensions/UIView+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Ext.swift
+//  FeatureImplTest
+//
+//  Created by Hugo Flores Perez on 5/20/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addToAndFill(parent: UIView) {
+        parent.addSubview(self)
+        self.trailingAnchor.constraint(equalTo: parent.trailingAnchor).isActive = true
+        self.leadingAnchor.constraint(equalTo: parent.leadingAnchor).isActive = true
+        self.topAnchor.constraint(equalTo: parent.topAnchor).isActive = true
+        self.bottomAnchor.constraint(equalTo: parent.bottomAnchor).isActive = true
+    }
+}
