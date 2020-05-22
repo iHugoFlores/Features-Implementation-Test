6b168b080b353160c92f2fdf041c381da3dfdeac
Hugo Flores <hfp.ing2@gmail.com>
2020-05-22 12:12:52 -0400

    Landing tab screen protocol

---
+++ FeatureImplTest/Utilities/Protocols/LandingTabScreen.swift
@@ -0,0 +1,13 @@
+//
+//  LandingTabScreenProtocol.swift
+//  FeatureImplTest
+//
+//  Created by Hugo Flores Perez on 5/22/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import UIKit
+
+protocol LandingTabScreen {
+    func getRightNavbarIconButton() -> UIBarButtonItem
+}
